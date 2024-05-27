b44ee2fdc4cc2c283f93706dcda1ed73aae698a5
HidekiKoyama <koyama200428@gmail.com>
2024-05-26 21:35:55 -0300

    realiza consulta apenas se o cnpj for valido

---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+from .api.validadorCNPJ import ValidarCnpj
--- src/app/app_main.py
+++ src/app/app_main.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request
 from src.app.utils.consulta_api import Consulta
+from src import ValidarCnpj
 
 app_main = Blueprint("app_main", __name__, static_folder="./view/", template_folder="./view/")
 
@@ -8,7 +9,13 @@ def index():
     if request.method == "POST":
         b = Consulta()
         cnpj = str(request.form.get("cnpj-busca"))
-        dados = b.busca(cnpj)
-        return render_template('index.html', dados=dados)
+        resul = (ValidarCnpj(cnpj).cnValidar())
+        if resul:  
+            dados = b.busca(cnpj)
+            return render_template('index.html', dados=dados)
+        else:
+            return render_template('index.html', dados="")
+
     else:
         return render_template('index.html', dados="")
+    
